053ec404cecd90799eb0cf33e8340ceaf74504af
Anurag Singh <anursingh28@gmail.com>
2018-12-25 04:35:53 +0530

    media url dispaly in browser

--- projecthunt/urls.py
+++ projecthunt/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from products import views
+
 
 
 urlpatterns = [
@@ -23,4 +26,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
