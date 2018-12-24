0770b2b606602467a501371a8f581c7f7ed4ec84
Anurag Singh <anursingh28@gmail.com>
2018-12-25 03:57:54 +0530

    Product create

--- products/views.py
+++ products/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
     return render(request,'products/home.html')
+
+@login_required()
+def create(request):
+    return render(request,'products/create.html')
